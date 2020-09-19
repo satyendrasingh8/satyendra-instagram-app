8bcf998fab05b54ff579f02bcdaacbf6ef10eabb
satyendrasingh8 <satyendrasinghsss7398@gmail.com>
2020-09-19 20:21:55 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:buddiesgram/pages/HomePage.dart';
+import 'package:flutter/material.dart';
+
+void main()
+{
+  WidgetsFlutterBinding.ensureInitialized();
+
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'instagram',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData
+      (
+        scaffoldBackgroundColor: Colors.black,
+        dialogBackgroundColor: Colors.black,
+        primarySwatch: Colors.grey,
+        cardColor: Colors.white70,
+        accentColor: Colors.black,
+      ),
+      home:HomePage(),
+    );
+  }
+}
---
+++ lib/pages/HomePage.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  bool isSignedIn = false;
+  Widget buildHomeScreen(){
+    return Text('already signed in');
+  }
+
+  Scaffold buildSignInScreen(){
+    return Scaffold(
+      body:Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topRight,
+            end:  Alignment.bottomLeft,
+            colors:[Theme.of(context).accentColor,Theme.of(context).primaryColor]
+
+          )
+        ),
+        alignment: Alignment.center,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text('instagram',
+              style: TextStyle(fontSize:92 ,color: Colors.white,fontFamily: "Signatra"),),
+            GestureDetector(
+              onTap: ()=> "button tapped",
+              child: Container(
+                width: 270,
+                height: 65,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('assets/images/google_signin_button.png'),
+                    fit: BoxFit.cover,
+                  )
+                ),
+              ),
+            ),
+
+          ],
+        ),
+      )
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+   if(isSignedIn)
+     {
+     return buildHomeScreen();
+     } else {
+     return buildSignInScreen();
+   }
+  }
+}
