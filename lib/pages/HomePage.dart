347ea86b72c7dddad4426eabb5d6a838a09aae70
satyendrasingh8 <satyendrasinghsss7398@gmail.com>
2020-09-20 09:41:09 +0530

    home screen bottom navigation

--- lib/pages/HomePage.dart
+++ lib/pages/HomePage.dart
@@ -1,3 +1,8 @@
+import 'package:buddiesgram/pages/NotificationsPage.dart';
+import 'package:buddiesgram/pages/ProfilePage.dart';
+import 'package:buddiesgram/pages/SearchPage.dart';
+import 'package:buddiesgram/pages/TimeLinePage.dart';
+import 'package:buddiesgram/pages/UploadPage.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
@@ -11,13 +16,92 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   bool isSignedIn = false;
+  PageController pageController;
+  int getPageIndex = 0;
+  void initState(){
+  super.initState();
+  gSignIn.onCurrentUserChanged.listen((gSigninAccount){
+    controlSignIn(gSigninAccount);
+  }, onError:(gError){
+    print("Error Message: " +gError);
+  });
+
+  gSignIn.signInSilently(suppressErrors: false).then((gSignInAccount){
+    controlSignIn(gSignInAccount);
+  }).catchError((gError) {
+    print("Error Message: " + gError);
+  });
+  }
+  controlSignIn(GoogleSignInAccount signInAccount) async {
+    if(signInAccount != null)
+      {
+        setState(() {
+          isSignedIn =true;
+        });
+      } else{
+      setState(() {
+        isSignedIn = false;
+      });
+    }
+  }
+  void dispose() {
+    pageController.dispose();
+    super.dispose();
+  }
 
   loginUser() {
     gSignIn.signIn();
   }
 
-  Widget buildHomeScreen(){
-    return Text('already signed in');
+  logoutUser(){
+    gSignIn.signOut();
+  }
+  whenPageChanges(int pageIndex){
+   setState(() {
+     this.getPageIndex = pageIndex;
+   });
+  }
+
+onTapChangePage(int pageIndex  ){
+  setState(() {
+    this.getPageIndex = pageIndex;
+  });
+    pageController.animateToPage(pageIndex, duration: Duration(milliseconds: 400),
+        curve: Curves.bounceInOut,);
+}
+
+  Scaffold buildHomeScreen(){
+    return Scaffold(
+      body: PageView(
+        children: [
+          TimeLinePage(),
+          SearchPage(),
+          UploadPage(),
+          NotificationsPage(),
+          ProfilePage(),
+        ],
+        controller: pageController,
+        onPageChanged: whenPageChanges,
+        physics: NeverScrollableScrollPhysics(),
+      ),
+      bottomNavigationBar: CupertinoTabBar(
+        currentIndex: getPageIndex,
+        onTap: onTapChangePage,
+        backgroundColor: Theme.of(context).accentColor,
+        activeColor: Colors.white,
+        inactiveColor: Colors.blueGrey,
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home,)),
+          BottomNavigationBarItem(icon: Icon(Icons.search,)),
+          BottomNavigationBarItem(icon: Icon(Icons.photo_camera,size: 37.0,)),
+          BottomNavigationBarItem(icon: Icon(Icons.favorite,)),
+          BottomNavigationBarItem(icon: Icon(Icons.person,)),
+        ],
+      ),
+    );
+    // return RaisedButton.icon(onPressed: logoutUser, icon: Icon(
+    //   Icons.person_pin
+    // ), label: Text("Sign Out"));
   }
 
   Scaffold buildSignInScreen(){
