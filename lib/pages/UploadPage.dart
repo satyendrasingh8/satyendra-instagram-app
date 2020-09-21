574b1f07c97390500c59f6ea28de25448eade9dc
satyendrasingh8 <satyendrasinghsss7398@gmail.com>
2020-09-21 17:22:18 +0530

    post upload into firebse storage and firestore

--- lib/pages/UploadPage.dart
+++ lib/pages/UploadPage.dart
@@ -1,16 +1,33 @@
 
 import 'dart:io';
+import 'package:buddiesgram/models/user.dart';
+import 'package:buddiesgram/pages/HomePage.dart';
+import 'package:buddiesgram/widgets/ProgressWidget.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:image_picker/image_picker.dart';
-
+import 'package:path_provider/path_provider.dart';
+import 'package:uuid/uuid.dart';
+import 'package:image/image.dart' as ImD;
 class UploadPage extends StatefulWidget {
+  final User gCurrentUser;
+
+  UploadPage({this.gCurrentUser});
+
   @override
   _UploadPageState createState() => _UploadPageState();
 }
 
-class _UploadPageState extends State<UploadPage> {
+class _UploadPageState extends State<UploadPage> with AutomaticKeepAliveClientMixin<UploadPage> {
 File file;
+bool uploading = false;
+String postId = Uuid().v4();
+
+ TextEditingController descriptionTextEdittingController = TextEditingController();
+TextEditingController locationTextEdittingController = TextEditingController();
   captureImageWithCamera() async{
     Navigator.pop(context);
     File imageFile = await ImagePicker.pickImage(source: ImageSource.camera,
@@ -84,10 +101,160 @@ cancelUpload(){
       ),
     );
   }
+  clearPostInfo(){
+    locationTextEdittingController.clear();
+    descriptionTextEdittingController.clear();
+    setState(() {
+      file = null;
+    });
+}
+
+getUserCurrentLocation() async {
+    Position position = await Geolocator().getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+    List<Placemark> placeMarks = await Geolocator().placemarkFromCoordinates(position.latitude, position.longitude);
+    Placemark mPlaceMark = placeMarks[0];
+    String completeAddressInfo = '${mPlaceMark.subThoroughfare} ${mPlaceMark.thoroughfare}, ${mPlaceMark.subLocality} ${mPlaceMark.locality}, ${mPlaceMark.subAdministrativeArea} ${mPlaceMark.administrativeArea}, ${mPlaceMark.postalCode} ${mPlaceMark.country},';
+  String specificAddress ='${mPlaceMark.locality}, ${mPlaceMark.country}';
+  locationTextEdittingController.text = specificAddress;
+
+  }
+  compressingPhoto() async{
+    final tDirectory = await getTemporaryDirectory();
+    final path = tDirectory.path;
+    ImD.Image mImageFile = ImD.decodeImage(file.readAsBytesSync());
+  final compressedImageFile = File('$path/img_$postId.jpg')..writeAsBytesSync(ImD.encodeJpg(mImageFile,quality: 90));
+  setState(() {
+    file = compressedImageFile;
+  });
+  }
+controlUploadAndSave() async {
+    setState(() {
+      uploading = true;
+    });
+    await compressingPhoto();
+   String downloadUrl = await uploadPhoto(file);
+   savePostInfoToFireStore(url:downloadUrl,location:locationTextEdittingController.text,description:descriptionTextEdittingController.text);
+   locationTextEdittingController.clear();
+   descriptionTextEdittingController.clear();
+   setState(() {
+     file = null;
+     uploading = false;
+     postId = Uuid().v4();
+   });
+  }
+
+savePostInfoToFireStore({String url, String location, String description}){
+    postsReference.document(widget.gCurrentUser.id).collection("usersPosts").document(postId).setData({
+      "postId":postId,
+      "ownerId":widget.gCurrentUser.id,
+      "timestamps": timestamp,
+      "likes": {},
+      "username": widget.gCurrentUser.username,
+      "description": description,
+      "location": location,
+      "url": url,
+    });
+}
 
 
+ Future<String> uploadPhoto(mImageFile) async {
+    StorageUploadTask mStorageUploadTask = storageReference.child("post_$postId.jpg").putFile(mImageFile);
+    StorageTaskSnapshot storageTaskSnapshot = await mStorageUploadTask.onComplete;
+    String downloadUrl = await storageTaskSnapshot.ref.getDownloadURL();
+    return downloadUrl;
+  }
+
+displayUploadForm(){
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        leading: IconButton(icon: Icon(Icons.arrow_back, color: Colors.white,),onPressed: clearPostInfo,),
+        title: Text("New Post",style: TextStyle(fontSize: 24.0,color: Colors.white,fontWeight: FontWeight.bold),),
+        actions: [
+          FlatButton(
+            onPressed:uploading ? null: ()=> controlUploadAndSave() ,
+            child: Text("Share",style: TextStyle(
+              color: Colors.lightGreenAccent,fontWeight: FontWeight.bold, fontSize: 16.0,
+            ),),
+          )
+        ],
+      ),
+      body: ListView(
+
+        children: [
+          uploading ? linearProgress() :Text(""),
+          Container(
+            height: 230.0,
+            width: MediaQuery.of(context).size.width*0.8,
+            child: Center(
+              child: AspectRatio(
+                aspectRatio: 16/9,
+                child: Container(
+                  decoration: BoxDecoration(
+                    image: DecorationImage(image: FileImage(file),fit:BoxFit.cover,)
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(top: 12.0),
+          ),
+          ListTile(
+            leading: CircleAvatar(backgroundImage: CachedNetworkImageProvider(widget.gCurrentUser.url),),
+            title: Container(
+              width: 250.0,
+              child: TextField(
+                style: TextStyle(
+                  color: Colors.white
+                ),
+                controller: descriptionTextEdittingController,
+                decoration: InputDecoration(
+                  hintText: "Say something about yourself",
+                  hintStyle: TextStyle(color: Colors.white),
+                  border: InputBorder.none,
+                ),
+              ),
+            ),
+          ),
+          Divider(),
+          ListTile(
+            leading: Icon(Icons.person_pin_circle, color:Colors.white, size: 36.0,),
+            title: Container(
+              width: 250.0,
+              child: TextField(
+                style: TextStyle(
+                    color: Colors.white
+                ),
+                controller: locationTextEdittingController,
+                decoration: InputDecoration(
+                  hintText: "Write the location here.",
+                  hintStyle: TextStyle(color: Colors.white),
+                  border: InputBorder.none,
+                ),
+              ),
+            ),
+          ),
+          Container(
+            width: 220.0,
+            height: 110.0,
+            alignment: Alignment.center,
+            child: RaisedButton.icon(
+            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(35.0)),
+              color: Colors.green,
+              icon: Icon(Icons.location_on,color: Colors.white,),
+                label: Text("Get current location", style: TextStyle(color: Colors.white),),
+            onPressed: getUserCurrentLocation,
+            ),
+          )
+        ],
+      ),
+    );
+}
+
+bool get wantKeepAlive => true;
   @override
   Widget build(BuildContext context) {
-    return displayUploadScreen();
+    return file == null ? displayUploadScreen() : displayUploadForm();
   }
 }
